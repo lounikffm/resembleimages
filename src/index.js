982a8ff5326ba4becda7ece22a8b8748d7eb35bd
Louis Schuhmacher <lschuhmacher@comwrap.com>
2017-12-13 11:32:12 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const glob = require("glob")
+const renameExtension = require('rename-extension')
+
+const resembleImages = function(pattern, cwd) {
+
+	let paths = glob.sync(pattern, {cwd});
+
+	paths.map((refPath) => {
+
+		const testPath = refPath.replace('reference', 'test');
+		const diffPath = refPath.replace('reference', 'diff');
+
+		looksSame.createDiff({
+			reference: refPath,
+			current: testPath,
+			diff: diffPath,
+			highlightColor: '#ff00ff', //color to highlight the differences
+			strict: false,//strict comparsion
+			tolerance: 2.5
+		}, function(error) {});
+
+	})
+
+}
+
+module.exports = {
+		test   : (pattern, cwd) => resembleImages(pattern, cwd)
+}
